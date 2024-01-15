4b4af2e71b1b09d4d2bd66132db0c5e9e685924c
Le Hoang Dung <hoangdungle36@gmail.com>
2024-01-15 22:17:40 +0700

    debug p 48

--- fastsam/predict.py
+++ fastsam/predict.py
@@ -43,6 +43,7 @@ class FastSAMPredictor(DetectionPredictor):
             print(img_test.shape)
             img_test = img_test.transpose(0, 2)
             print(type(img_test))
+            img_test = img_test.cpu()
             cv2.imwrite("anhgoc.jpg", img_test)
             # print(len(p[0][1]))s
             for box in p[0]:
