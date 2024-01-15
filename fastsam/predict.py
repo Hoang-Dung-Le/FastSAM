6b5f5d57847563d829e7515272ab52bc9761bfdf
Le Hoang Dung <hoangdungle36@gmail.com>
2024-01-15 21:56:18 +0700

    debug p 37

--- fastsam/predict.py
+++ fastsam/predict.py
@@ -59,8 +59,9 @@ class FastSAMPredictor(DetectionPredictor):
                 cropped = img_test[y1:y2, x1:x2]
                 print(cropped.shape)
                 cropped = cropped.cpu().numpy()
-                # cv2.imwrite('/content/{x1}', cropped)
-                plt.imshow(cropped)
+                print(type(cropped))
+                cv2.imwrite('/content/{x1}', cropped)
+                # plt.imshow(cropped)
 
         except Exception as e:
             print(e)
