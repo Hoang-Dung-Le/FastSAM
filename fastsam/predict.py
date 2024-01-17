4b6e873f9bd05805e22b489f0e088e6a42f9843a
Le Hoang Dung <hoangdungle36@gmail.com>
2024-01-17 09:15:29 +0700

    debug p 89

--- fastsam/predict.py
+++ fastsam/predict.py
@@ -36,37 +36,21 @@ class FastSAMPredictor(DetectionPredictor):
                                     classes=self.args.classes)
         
         try:
-
-            # img_test = img[0]
-            # img_test = img_test.cpu().numpy()
-
-            # box = p[0][critical_iou_index]
-            # print(box)
-            # box = box.squeeze()
-            # box = box.cpu().numpy()
-            # print(box.shape)
-            # x1, y1, x2, y2 = box[:4].astype(int)
-            # cropped = img_test[y1:y2, x1:x2]
-            # cv2.imwrite(f'/content/{2}.jpg', cropped * 255)
-            # p1 = [tensor.cpu().numpy() for tensor in p]
-            # cropped_imgs = [] 
-
             img_test = img[0]
             img_test = img_test.cpu().numpy()
-            print(p[0])
+            # print(p[0])
             img_test = np.transpose(img_test, (2, 1, 0))
             for box in p[0]:
                 box = box.cpu().numpy()
-                x1, y1, x2, y2 = box[:4].astype(int)  # Đảm bảo thứ tự tọa độ chính xác
+                x1, y1, x2, y2 = box[:4].astype(int)
 
-                # Cắt ảnh từ box
+            
                 if x1 < 0 or y1 < 0 or x2 > img_test.shape[1] or y2 > img_test.shape[0]:
                     print("Xoá bounding box vì tọa độ nằm ngoài ảnh")
                     continue
                 cropped = img_test[y1:y2, x1:x2]
-                # cropped = cropped
                 cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)
-                cv2.imwrite(f'/content/{x1}.jpg', cropped * 255)
+                cv2.imwrite(f'/content/drive/MyDrive/CV/fastsam/test_dog_imgs/{x1}.jpg', cropped * 255)
                 plt.imshow(cropped)
 
         except Exception as e:
