55e8376a03b0ab7649954d3c690620af8bbe0dda
Le Hoang Dung <hoangdungle36@gmail.com>
2024-01-22 09:18:08 +0700

    + debug resnet 26

--- fastsam/predict.py
+++ fastsam/predict.py
@@ -37,6 +37,7 @@ class FastSAMPredictor(DetectionPredictor):
 
     def predict(self, image):
         try:
+            print("ok")
             input_tensor = self.transform(image)
             print("ok1")
             input_batch = input_tensor.unsqueeze(0)  # Thêm chiều batch
