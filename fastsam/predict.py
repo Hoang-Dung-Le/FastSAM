f03eff837a302c729c6ecce3adb6cb00b70265db
Le Hoang Dung <hoangdungle36@gmail.com>
2024-01-22 13:26:36 +0700

    + debug resnet 58

--- fastsam/predict.py
+++ fastsam/predict.py
@@ -88,7 +88,7 @@ class FastSAMPredictor(DetectionPredictor):
                     continue
                 cropped = img_test[y1:y2, x1:x2]
 
-                cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)
+                # cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)
 
                 # cropped = torch.from_numpy(cropped)  # Uncomment if needed for prediction
                 pred = self.predict(cropped)
