ce1556a1e13332554cc9ffa1fa3ea50d88f562b3
Le Hoang Dung <hoangdungle36@gmail.com>
2024-01-23 21:48:58 +0700

    + debug resnet 136

--- fastsam/predict.py
+++ fastsam/predict.py
@@ -116,6 +116,8 @@ class FastSAMPredictor(DetectionPredictor):
                     if prediction == 1:
                         item = item.cuda()  
                         kept_boxes = torch.cat([kept_boxes, pred[i].unsqueeze(0)])
+
+                print("ok")
                 try:
                     masks = ops.process_mask_native(proto[i], pred[:, 6:], pred[:, :4], orig_img.shape[:2])  # HWC
                 except Exception as e:
