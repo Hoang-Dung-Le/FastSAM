584f70d144fdeb0cca0d9f569c14df24d16d3cc4
Le Hoang Dung <hoangdungle36@gmail.com>
2024-01-23 17:42:40 +0700

    + debug resnet 96

--- fastsam/predict.py
+++ fastsam/predict.py
@@ -147,7 +147,7 @@ class FastSAMPredictor(DetectionPredictor):
 
                             print(cropped_img.shape)
 
-                            cv2.imwrite(f"/content/{x1}.png", cropped_img)
+                            cv2.imwrite(f"/content/sdff{x1}.png", cropped_img)
                     except Exception as e:
                         print(e)
                 masks = ops.process_mask_native(proto[i], pred[:, 6:], pred[:, :4], orig_img.shape[:2])  # HWC
