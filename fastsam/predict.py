8b3526f1d105e708a4ae3712ab3a3d8f5bf89993
Le Hoang Dung <hoangdungle36@gmail.com>
2024-01-24 09:35:29 +0700

    + debug resnet 163

--- fastsam/predict.py
+++ fastsam/predict.py
@@ -34,7 +34,7 @@ class FastSAMPredictor(DetectionPredictor):
         self.model = self._load_model('/content/drive/MyDrive/CV/fastsam/classifier_checkpoint/model_resnet34.pth', 2)
         self.transform = transforms.Compose([
             transforms.Resize((224, 224), antialias=True),
-            transforms.ToTensor(),
+            # transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
