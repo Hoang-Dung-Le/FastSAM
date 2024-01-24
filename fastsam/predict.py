a73078929b042fdb7d1fe24f08236c41436759d6
Le Hoang Dung <hoangdungle36@gmail.com>
2024-01-24 09:28:52 +0700

    + debug resnet 161

--- fastsam/predict.py
+++ fastsam/predict.py
@@ -129,6 +129,7 @@ class FastSAMPredictor(DetectionPredictor):
                         x1, y1, x2, y2 = box_np[:4].astype(int)
                         cropped_img = orig_img[y1:y2, x1:x2]
                         cropped_img = cropped_img / 255.
+                        prediction = self.predict(cropped_img)
                 except Exception as e:
                     print("loi ne ", e)
 
