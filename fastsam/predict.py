c70fe560a7258ca5a92eb3894baf4ef2fcca9595
Le Hoang Dung <hoangdungle36@gmail.com>
2024-01-24 10:18:20 +0700

    + debug resnet 166

--- fastsam/predict.py
+++ fastsam/predict.py
@@ -54,7 +54,6 @@ class FastSAMPredictor(DetectionPredictor):
         self.model.load_state_dict(torch.load('/content/drive/MyDrive/CV/fastsam/classifier_checkpoint/model_custom.pth'))
         self.transform = transforms.Compose([
             transforms.Resize((224, 224)),
-            transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
         image = image.transpose((2, 0, 1))
