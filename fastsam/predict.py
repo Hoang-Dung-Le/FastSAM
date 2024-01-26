cf5ea704b8f0129344b2d6a5c72204bffa01f1ab
Le Hoang Dung <hoangdungle36@gmail.com>
2024-01-26 16:45:13 +0700

    + debug 8

--- fastsam/predict.py
+++ fastsam/predict.py
@@ -144,7 +144,7 @@ class FastSAMPredictor(DetectionPredictor):
                         cropped_img = cropped_img / 255.
                         prediction = self.predict(cropped_img)
               
-                        if prediction == 7846:
+                        if prediction == 537:
                 #             # pr = pr.cuda()  
                             kept_boxes = torch.cat([kept_boxes, test[idx].unsqueeze(0)])
                         # print(prediction)
