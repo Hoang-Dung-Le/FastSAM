a290c2fac5f45d3a603654594e1427b3d9678602
Le Hoang Dung <hoangdungle36@gmail.com>
2024-01-31 19:12:57 +0700

    + test img 20

--- fastsam/predict.py
+++ fastsam/predict.py
@@ -142,9 +142,9 @@ class FastSAMPredictor(DetectionPredictor):
                         box_np = pr.detach().cpu().numpy()
                         x1, y1, x2, y2 = box_np[:4].astype(int)
                         cropped_img = orig_img[y1:y2, x1:x2]
-                        path = self.output + f"/img{x1}.png"
+                        path = self.output + f"img{x1}.png"
                         # print(path)
-                        # cv2.imwrite(path, cropped_img)
+                        cv2.imwrite(path, cropped_img)
                         cropped_img = cropped_img / 255.
                         prediction = self.predict(cropped_img)
                         # print(prediction)
